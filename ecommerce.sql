5aed5e13adc908cd51ef1373290138b055ecf859
Vastagon <77780118+Vastagon@users.noreply.github.com>
2023-04-13 18:43:52 -0500

    Update ecommerce.sql

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,7 +1,7 @@
 CREATE TABLE items (
   id BIGSERIAL NOT NULL PRIMARY KEY,
   title VARCHAR(250) NOT NULL,
-  description VARCHAR(500),
+  item_description VARCHAR(500),
   rating INT,
   price float,
   created_at timestamp 
@@ -34,4 +34,14 @@ CREATE TABLE cartitems (
   final_total_purchase_cost float /* Total cost of the item after tax and everything else. Can reference this for previous purchases if prices change, this amount stays the same. */
 );
 
+
+-- INSERT INTO cart(user_id,created_at) VALUES (1, NOW());
+
+
+-- SELECT * FROM cartitems WHERE cart_id = 1;
+
+
+-- SELECT * FROM cart JOIN cartitems ON cart.id = cartitems.cart_id;
+
+-- SELECT person.first_name, car.make, car.model, car.price FROM person JOIN car ON person.car_id = car.id;
 
